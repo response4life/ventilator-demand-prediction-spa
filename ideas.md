1644f228a226f582573834d96a11d59c9eed963b
Ian Brennan <ian.brennan@viget.com>
2020-03-21 11:23:37 -0500

    basic victory chart of SIR and ventilator data

---
+++ ideas.md
@@ -0,0 +1,4 @@
+## Enhancements
+
+- ability to turn lines on graph on and off
+- ability to adjust parameters
--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
+
+import { VictoryLine, VictoryChart } from 'victory'
 
 function App() {
+  const [ventilatorData, setVentilatorData] = useState([])
+  const [infectedData, setInfectedData] = useState([])
+  const [susceptibleData, setSusceptibleData] = useState([])
+  const [recoveredData, setRecoveredData] = useState([])
+  const [hospitalizedData, setHospitalizedData] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios({
+        method: 'get',
+        url: 'http://localhost:5000/'
+      })
+      const ventilatorData = result.data.map(day => ({
+        x: day.day,
+        y: day.ventilators_needed
+      }))
+      const infectedData = result.data.map(day => ({
+        x: day.day,
+        y: day.infected
+      }))
+      const hospitalizedData = result.data.map(day => ({
+        x: day.day,
+        y: day.hospitalized
+      }))
+      const susceptibleData = result.data.map(day => ({
+        x: day.day,
+        y: day.susceptible
+      }))
+      const recoveredData = result.data.map(day => ({
+        x: day.day,
+        y: day.recovered
+      }))
+      setVentilatorData(ventilatorData)
+      setInfectedData(infectedData)
+      setHospitalizedData(hospitalizedData)
+      setSusceptibleData(susceptibleData)
+      setRecoveredData(recoveredData)
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ height: '100%', width: '70%', margin: 'auto' }}>
+        <VictoryChart>
+          <VictoryLine data={ventilatorData} />
+          <VictoryLine data={infectedData} />
+          <VictoryLine data={hospitalizedData} />
+          <VictoryLine data={susceptibleData} />
+          <VictoryLine data={recoveredData} />
+        </VictoryChart>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
